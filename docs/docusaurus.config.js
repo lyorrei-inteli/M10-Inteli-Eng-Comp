4ff3208463c337c0a765ee69bc3721ff58309af4
Murilo-ZC <carvalho.zanini@gmail.com>
2024-04-15 09:59:33 -0300

    ajustado o baseUrl

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -13,10 +13,10 @@ const config = {
   favicon: 'img/inteli.svg',
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://murilo-zc.github.io/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/M10-Inteli-Eng-Comp/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
