76560d7d8ea71331c604786bf46f05331dae4f88
Murilo-ZC <carvalho.zanini@gmail.com>
2024-05-06 13:01:42 -0300

    adicionado base do encontro 06

--- src/encontro05/aplicacao01/src/app.py
+++ src/encontro05/aplicacao01/src/app.py
@@ -91,4 +91,4 @@ def deletar_usuario(id):
     return '', 204
 
 if __name__ == '__main__':
-    app.run(debug=False, host='0.0.0.0', port=8000)
+    app.run(debug=False, host='0.0.0.0', port=8000, threaded=False)
